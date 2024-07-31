1637f7fca1e6f41b65665e0e907f4aec719ae5e8
Himanshu Kumar <himanshu7277069@gmail.com>
2024-08-01 00:16:15 +0530

    footer update

--- src/componenets/footer/Footer.jsx
+++ src/componenets/footer/Footer.jsx
@@ -22,48 +22,20 @@ function Footer() {
 
                 {/* items  */}
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-                    © 2023 Blogram —
+                    © 2024 Blogram —
                     <a
                         href="#"
                         className="text-gray-600 ml-1"
                         rel="noopener noreferrer"
                         target="_blank"
                     >
-                        @Himanshu
+                        made with &#10084; by @Himanshu
                     </a>
                 </p>
                 
                 {/* Right item  */}
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    {/* Icon 1  */}
-                    <a className="text-gray-500">
-                        <svg
-                            fill="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            className="w-5 h-5"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                        </svg>
-                    </a>
-
-                    {/* Icon 2  */}
-                    <a className="ml-3 text-gray-500">
-                        <svg
-                            fill="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            className="w-5 h-5"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                        </svg>
-                    </a>
-
-                    {/* Icon 3  */}
+                    {/* Icon 1 */}
                     <a className="ml-3 text-gray-500">
                         <svg
                             fill="none"
@@ -79,7 +51,7 @@ function Footer() {
                         </svg>
                     </a>
 
-                    {/* Icon 4  */}
+                    {/* Icon 2 */}
                     <a className="ml-3 text-gray-500">
                         <svg
                             fill="currentColor"
